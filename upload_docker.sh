8a2e7d6ad98a13f5a93c1bd80922218393471192
tarungirdhar1980 <86316947+tarungirdhar1980@users.noreply.github.com>
2021-09-12 22:42:50 +0530

    Add files via upload

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,7 +3,7 @@
 # Assumes that an image is built via `run_docker.sh`
 
 # Step 1: Create dockerpath
-dockerpath="visheshvashista/visheshcapstone"
+dockerpath="tarungirdhar1980/tarunfinalproject"
 
 # Step 2: Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
